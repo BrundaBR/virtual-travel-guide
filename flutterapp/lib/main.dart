0b0a22e9c1f1f7bf0ba8f4cf55caef8a47a6ffb5
BrundaBR <revadibrunda@gmail.com>
2020-10-02 11:49:11 +0530

    homepage basic template

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'pages/second.dart';
+
 void main()=>runApp(Homepage());
 
 
@@ -8,7 +9,7 @@ class Homepage  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Basic App',
+      title: 'Virtual Guide',
       debugShowCheckedModeBanner: false,
       home: FirstPage (),
     );
@@ -23,19 +24,85 @@ class FirstPage  extends StatelessWidget{
     // TODO: implement build
     return Scaffold(
 
+      backgroundColor:Colors.blueGrey,
 
-      body:ListView(
+      body:SafeArea(
+      child:Stack(
+      children:<Widget>[
 
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text('Home',style: TextStyle(color:Colors.white,fontSize: 35,fontFamily: 'Verdana'),),
+          ),
+        ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children:<Widget>[
+
+
+          Container(
+            height: 500,
+decoration: BoxDecoration(
+  color:Colors.white,
+  borderRadius: BorderRadius.only(
+    topRight: Radius.circular(40),
+    topLeft: Radius.circular(40),
+  ),
+  
+),
+            
+            
+
+
+
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children:<Widget>[
+              Container(
+                alignment: AlignmentDirectional.center,
+                //image
+                height:90,
+
+                child:Text(""),
+              ),
+                Container(
+                  //button
+                  padding: const EdgeInsets.all(50.0),
+                  child:FlatButton(
+                    color:Colors.blueGrey,
+
+                  child: Text('Location',style: TextStyle(color:Colors.white,fontSize: 25,fontFamily: 'Verdana'),),
+
+                    onPressed: (){
+                      Navigator.push(
+                      context,
+
+                       MaterialPageRoute(builder: (context) => SecondPage()),
+                      );
+
+                    },
+
+                  ),
+
+                ),
+              ],
+
+            ),
+
+          )
+    ]
+          ),
+        ],
       ),
+
+
+)
     );
 
     }
 }
 
-
-//  onPressed: ()
-// {
-//   Navigator.push(
-//     context,
-//     MaterialPageRoute(builder: (context) => SecondPage()),
-//   );
